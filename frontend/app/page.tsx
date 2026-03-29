ca7f86a5458893d226000a7007364ba8227732c8
sumail829 <samirpanjiyar4@gmail.com>
2026-03-29 23:30:29 +0545

    frontend setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-4xl text-white font-bold">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  );
+}
